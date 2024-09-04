a082f1db143a9b6eabc0197b83d700ce5866ea64
Abdosamiir <barakatabdo82@gmail.com>
2024-09-04 23:58:01 +0300

    fixed some errors

--- the-wild-oasis/src/features/authentication/LoginForm.jsx
+++ the-wild-oasis/src/features/authentication/LoginForm.jsx
@@ -7,8 +7,8 @@ import FormRowVertical from "../../ui/FormRowVertical";
 import { useLogin } from "./useLogin";
 import SpinnerMini from "../../ui/SpinnerMini";
 function LoginForm() {
-  const [email, setEmail] = useState("abdo@example.com");
-  const [password, setPassword] = useState("123456");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
   const { login, isLoading } = useLogin();
 
   function handleSubmit(e) {
