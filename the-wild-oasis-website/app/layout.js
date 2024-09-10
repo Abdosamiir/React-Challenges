50c1b995947a46dea5c17c4508b0b22ed3a3a87e
Abdosamiir <barakatabdo82@gmail.com>
2024-09-10 08:10:27 +0300

    add metadata , fonts

--- the-wild-oasis-website/app/layout.js
+++ the-wild-oasis-website/app/layout.js
@@ -1,14 +1,27 @@
 import Logo from "@/app/_components/Logo";
 import Navigaton from "@/app/_components/Navigaton";
 import "@/app/_styles/globals.css";
+import { Josefin_Sans } from "next/font/google";
+
+const josefin = Josefin_Sans({
+  subsets: ["latin"],
+  display: "swap",
+});
+
 export const metadata = {
-  title: "The Wild Oasis",
+  title: {
+    template: "%s | The Wild Oasis",
+    default: "Welcome To The Wild Oasis",
+  },
+  description: "Welcome to The Wild Oasis Luxury Cabin Rental.",
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className="bg-primary-950 text-primary-50 min-h-screen">
+      <body
+        className={`${josefin.className} bg-primary-950 text-primary-50 min-h-screen`}
+      >
         <header>
           <Logo />
           <Navigaton />
