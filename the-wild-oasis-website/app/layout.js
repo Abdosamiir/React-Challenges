b12a9ca83384a70054f7ae53e655538ad22340ea
Abdosamiir <barakatabdo82@gmail.com>
2024-09-10 09:34:55 +0300

    impelement cabins and about page

--- the-wild-oasis-website/app/layout.js
+++ the-wild-oasis-website/app/layout.js
@@ -1,7 +1,6 @@
-import Logo from "@/app/_components/Logo";
-import Navigaton from "@/app/_components/Navigaton";
 import "@/app/_styles/globals.css";
 import { Josefin_Sans } from "next/font/google";
+import Header from "./_components/Header";
 
 const josefin = Josefin_Sans({
   subsets: ["latin"],
@@ -20,13 +19,12 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body
-        className={`${josefin.className} bg-primary-950 text-primary-50 min-h-screen`}
+        className={`${josefin.className} antialiased bg-primary-950 text-primary-50 min-h-screen flex flex-col relative`}
       >
-        <header>
-          <Logo />
-          <Navigaton />
-        </header>
-        <main>{children}</main>
+        <Header />
+        <div className="flex-1 px-8 py-12">
+          <main className="mx-auto max-w-7xl">{children}</main>
+        </div>
       </body>
     </html>
   );
