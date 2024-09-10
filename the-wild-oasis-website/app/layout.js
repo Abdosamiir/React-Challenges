cc9de71aa5ba6a07ab34758ce588b44784a892e7
Abdosamiir <barakatabdo82@gmail.com>
2024-09-10 20:57:50 +0300

    impelement cabins page

--- the-wild-oasis-website/app/layout.js
+++ the-wild-oasis-website/app/layout.js
@@ -12,18 +12,20 @@ export const metadata = {
     template: "%s | The Wild Oasis",
     default: "Welcome To The Wild Oasis",
   },
-  description: "Welcome to The Wild Oasis Luxury Cabin Rental.",
+  description:
+    "Luxurious cabin hotel, located in the heart of the Italian Dolomites, surrounded by beautiful mountains and dark forests.",
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body
-        className={`${josefin.className} antialiased bg-primary-950 text-primary-50 min-h-screen flex flex-col relative`}
+        className={`${josefin.className} antialiased bg-primary-950 text-primary-100 min-h-screen flex flex-col relative`}
       >
         <Header />
-        <div className="flex-1 px-8 py-12">
-          <main className="mx-auto max-w-7xl">{children}</main>
+
+        <div className="flex-1 px-8 py-12 grid">
+          <main className="max-w-7xl mx-auto w-full">{children}</main>
         </div>
       </body>
     </html>
