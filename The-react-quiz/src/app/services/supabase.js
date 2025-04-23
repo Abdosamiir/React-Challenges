74bb57f74e07cf7e49127a41c0bbdd23ccc3b1dd
abdosamiir <barakatabdo82@gmail.com>
2025-04-23 18:15:25 +0200

    replace supabase key retrieval with hardcoded key for testing purposes

--- The-react-quiz/src/app/services/supabase.js
+++ The-react-quiz/src/app/services/supabase.js
@@ -1,7 +1,8 @@
 import { createClient } from "@supabase/supabase-js";
 const supabaseUrl = "https://ihtetjxwdwmxzuodchwu.supabase.co";
-// eslint-disable-next-line no-undef
-const supabaseKey = process.env.VITE_SUPABASE_KEY;
+const supabaseKey =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImlodGV0anh3ZHdteHp1b2RjaHd1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDU0MTYxODIsImV4cCI6MjA2MDk5MjE4Mn0.LsgJtfC305uDvQtjOT-aEfuby4nY0BdzHiosKaDgX0A";
+
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 export default supabase;
